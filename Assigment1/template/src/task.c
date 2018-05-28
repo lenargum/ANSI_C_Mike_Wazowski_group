4f0a93379bbb08d0844a70293d75f15c805f46b1
Igor <igor.krash@gmail.com>
2018-05-28 23:05:21 +0300

    +3-3

--- Assigment1/template/src/task.c
+++ Assigment1/template/src/task.c
@@ -3,6 +3,7 @@
 //
 
 #include <string.h>
+#include <ctype.h>
 #include "task.h"
 
 /** The stub function
@@ -252,6 +253,31 @@ int any(const char s1[], const char s2[]) {
         }
     }
     return -1;
+}
+
+char* expand(const char s1[]){
+    int i, j;
+    char t;
+    char * s2 = malloc(sizeof(char));
+    for (i = 0, j = 0; s1[i] != '\0'; i++)
+    {
+        if ( s1[i] == '-' )
+        {
+            if ( (isdigit(s1[i-1]) && isdigit(s1[i+1]) && (s1[i-1]<s1[i+1])) ||
+                 (islower(s1[i-1]) && islower(s1[i+1]) && (s1[i-1]<s1[i+1])) ||
+                 (isupper(s1[i-1]) && isupper(s1[i+1]) && (s1[i-1]<s1[i+1])) )
+                for (t = (char)(s1[i-1]+1); t < s1[i+1]; t++){
+                    s2 = realloc(s2, sizeof(char) * (i + t));
+                    s2[j++] = t;
+                }
+            else
+                s2[j++] = s1[i];
+        }
+        else
+            s2[j++] = s1[i];
+    }
+    s2[j] = '\0';
+    return s2;
 }
 
 char* itoa(int n) {
