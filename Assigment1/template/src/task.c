f7168a9201abcdcf056779209b7bc856776f2d6b
Lenar <lenargamgam@gmail.com>
2018-05-28 23:01:06 +0300

    binsearch

--- Assigment1/template/src/task.c
+++ Assigment1/template/src/task.c
@@ -254,6 +254,25 @@ int any(const char s1[], const char s2[]) {
     return -1;
 }
 
+int binsearch(int x, int v[], int n) {
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+    while (low < high) {
+        mid = (low + high) / 2;
+        if (x <= v[mid])
+            high = mid;
+        else
+            low = mid + 1;
+    }
+    if (v[low] == x) {
+        return low;
+    } else {
+        return -1;
+    }
+}
+
 char* itoa(int n) {
     int i, sign;
     sign = n;
