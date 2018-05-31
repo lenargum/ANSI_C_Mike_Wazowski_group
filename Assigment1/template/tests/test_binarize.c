fe7f63c4edcbf7c305454112beda48f958e1f366
artyom-yurin <artem_yrin@mail.ru>
2018-05-31 15:51:36 +0300

    Add template for second homework

--- Assigment1/template/tests/test_binarize.c
+++ Assigment1/template/tests/test_binarize.c
@@ -19,14 +19,14 @@
 #endif
 
 START_TEST (test_bin_u)
-{
-	unsigned long x = 1608637542;
-	char* output = binarize_u(x);
-	char* pattern = "01011111111000011101110001100110";
-	int k = 1;
-	COMPARATOR(k, output, pattern);
-	ck_assert(1 == k);
-}
+    {
+        unsigned long x = 1608637542;
+        char* output = binarize_u(x);
+        char* pattern = "01011111111000011101110001100110";
+        int k = 1;
+        COMPARATOR(k, output, pattern);
+        ck_assert(1 == k);
+    }
 END_TEST
 
 START_TEST (test_bin_u_border)
@@ -41,25 +41,25 @@ START_TEST (test_bin_u_border)
 END_TEST
 
 START_TEST (test_bin_s)
-{
-	signed long x = -1608637542;
-	char* output = binarize_s(x);
-	char* pattern = "10100000000111100010001110011010";
-	int k = 1;
-	COMPARATOR(k, output, pattern);
-	ck_assert(1 == k);
-}
+    {
+        signed long x = -1608637542;
+        char* output = binarize_s(x);
+        char* pattern = "10100000000111100010001110011010";
+        int k = 1;
+        COMPARATOR(k, output, pattern);
+        ck_assert(1 == k);
+    }
 END_TEST
 
 START_TEST (test_u_and_s)
-{
-	signed long x = 42;
-	char* output = binarize_s(x);
-	char* pattern = binarize_u(x);
-	int k = 1;
-	COMPARATOR(k, output, pattern);
-	ck_assert(1 == k);
-}
+    {
+        signed long x = 42;
+        char* output = binarize_s(x);
+        char* pattern = binarize_u(x);
+        int k = 1;
+        COMPARATOR(k, output, pattern);
+        ck_assert(1 == k);
+    }
 END_TEST
 
 START_TEST (test_bin_s_border)
@@ -74,23 +74,23 @@ START_TEST (test_bin_s_border)
 END_TEST
 
 Suite* str_suite (void) {
-	Suite *suite = suite_create("binarize");
-	TCase *tcase = tcase_create("case");
-	tcase_add_test(tcase, test_bin_u);
-	tcase_add_test(tcase, test_bin_s);
-	tcase_add_test(tcase, test_u_and_s);
-	tcase_add_test(tcase, test_bin_u_border);
-	tcase_add_test(tcase, test_bin_s_border);
-	suite_add_tcase(suite, tcase);
-	return suite;
+    Suite *suite = suite_create("binarize");
+    TCase *tcase = tcase_create("case");
+    tcase_add_test(tcase, test_bin_u);
+    tcase_add_test(tcase, test_bin_s);
+    tcase_add_test(tcase, test_u_and_s);
+    tcase_add_test(tcase, test_bin_u_border);
+    tcase_add_test(tcase, test_bin_s_border);
+    suite_add_tcase(suite, tcase);
+    return suite;
 }
 
 int main (int argc, char *argv[]) {
-	int number_failed;
-	Suite *suite = str_suite();
-	SRunner *runner = srunner_create(suite);
-	srunner_run_all(runner, CK_NORMAL);
-	number_failed = srunner_ntests_failed(runner);
-	srunner_free(runner);
-	return number_failed;
+    int number_failed;
+    Suite *suite = str_suite();
+    SRunner *runner = srunner_create(suite);
+    srunner_run_all(runner, CK_NORMAL);
+    number_failed = srunner_ntests_failed(runner);
+    srunner_free(runner);
+    return number_failed;
 }
---
+++ Assigment2/ha-2/tests/test_queue.c
@@ -0,0 +1,33 @@
+#include <check.h>
+#include "priority_queue.h"
+
+START_TEST (test_insert)
+{
+
+}
+END_TEST
+
+START_TEST (test_min)
+{
+
+}
+END_TEST
+
+Suite* str_suite (void) {
+	Suite *suite = suite_create("priority queue");
+	TCase *tcase = tcase_create("case");
+	tcase_add_test(tcase, test_insert);
+	tcase_add_test(tcase, test_min);
+	suite_add_tcase(suite, tcase);
+	return suite;
+}
+
+int main (int argc, char *argv[]) {
+	int number_failed;
+	Suite *suite = str_suite();
+	SRunner *runner = srunner_create(suite);
+	srunner_run_all(runner, CK_NORMAL);
+	number_failed = srunner_ntests_failed(runner);
+	srunner_free(runner);
+	return number_failed;
+}
